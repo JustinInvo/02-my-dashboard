afc5d62d3bdfa1b2b8dd9200987aa6bccf604150
Justin <jusrtin2010@gmail.com>
2025-11-30 18:24:56 -0500

    Add Pokémon dashboard components and error handling
    - Implemented Siderbar with new Pokémon menu item.
    - Created error boundary component for handling server errors.
    - Developed PokemonsPage to fetch and display Pokémon data.
    - Added PokemonCard and PokemonGrid components for rendering Pokémon.
    - Defined interfaces for Pokémon data structure.

---
+++ src/app/pokemons/components/PokemonGrid.tsx
@@ -0,0 +1,20 @@
+import { SimplePokemon } from '../interfaces/simple-pokemon'
+import { PokemonCard } from './PokemonCard'
+
+
+interface Props {
+  pokemons: SimplePokemon[];
+}
+
+export const PokemonGrid = ({ pokemons }: Props) => {
+  return (
+    <div className="flex flex-wrap gap-10 items-center justify-center">
+      {
+        pokemons.map( (pokemon, key) => (
+          <PokemonCard key={key} pokemon={pokemon}/>
+        ))
+      }
+      {/* { JSON.stringify(pokemons) } */}
+    </div>
+  )
+}
---
+++ src/app/pokemons/index.ts
@@ -0,0 +1,4 @@
+import { PokemonGrid } from './components/PokemonGrid';
+
+export type { PokemonsResponse } from './interfaces/pokemons-response'
+export type { SimplePokemon } from './interfaces/simple-pokemon'
